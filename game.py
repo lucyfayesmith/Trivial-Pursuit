a6eba104f7714d05cbbc6cfa1748f38c14413518
Lucy Smith <lucy.smith2@my.jcu.edu.au>
2018-08-16 13:58:00 +1000

    Commit game.py

--- game.py
+++ game.py
@@ -1,28 +1,47 @@
+from random import shuffle
+
+
 def main():
+    questions = load_questions()
+    for question in questions:
+        shuffled_options = shuffle_options(question)
+        print(shuffled_options)
+        ask_quesitons(question[0], shuffled_options)
 
 
-    questions_file = open("Questions","r")
+    ask_quesitons()
 
+def load_questions():
     questions = []
+    questions_file = open("Questions", "r")
 
     for line in questions_file:
         question = line.strip("\n").split(",")
         questions.append(question)
 
-    print(questions)
+    questions_file.close()
 
-    ask_questions()
+    return questions
 
-def ask_questions():
 
-    questions_file = open("Questions", "r")
+def shuffle_options(questions_row):
+    options = []
+    for i in range(1, 4):
+        options.append(questions_row[i])
+    shuffle(options)
+    return options
 
-    user_answer = []
 
-    for line in questions_file:
-        user_answer = input("What is your answer?"
-                            "\nA"
-                            "\nB"
-                            "\nC")
+def ask_quesitons(questions, answers):
+    print("{}? \nA){} \nB){} \nC){}".format(questions, answers[0], answers[1], answers[2]))
+
+    user_answer = ""
+
+    while user_answer != "A" and user_answer != "B" and user_answer != "C":
+        user_answer = input(">>>")
+        user_answer = user_answer.upper()
+
+    return user_answer
+
 
 main()
